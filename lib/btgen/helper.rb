6e7b7afd9a10696393fcced27f4ce10554e5c1b3
Manuel Bustillo <mayn13@gmail.com>
2017-08-12 20:45:59 +0200

    Add some helpers and dependency on bh gem

--- lib/btgen/helper.rb
+++ lib/btgen/helper.rb
@@ -7,5 +7,21 @@ module Btgen
       yield table
       table.to_html
     end
+
+    def panel_with_body(*args)
+      panel(*args) do
+        content_tag :div, class: 'panel-body' do
+          yield
+        end
+      end
+    end
+
+    def bootstrap_description(instance, attributes)
+      content_tag :dl, class: 'dl-horizontal' do
+        attributes.to_a.map do |(k, v)|
+          (content_tag :dt, instance.class.human_attribute_name(k)) + (content_tag :dd, v)
+        end.inject(:+)
+      end
+    end
   end
 end
