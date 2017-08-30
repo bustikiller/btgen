320c3230c6082b151a1ced821bb6bd83590b1985
Manuel Bustillo <mayn13@gmail.com>
2017-08-30 20:28:34 +0200

    Add support for collapsible panels

--- lib/btgen/helper.rb
+++ lib/btgen/helper.rb
@@ -8,9 +8,27 @@ module Btgen
       table.to_html
     end
 
+    def expand_collapsible_label(target_id)
+      content_tag(:span, ' + ', href: "##{target_id}", data: {toggle: 'collapse'})
+    end
+
     def panel_with_body(*args)
+
+      final_class = 'panel-body'
+      collapsible = args.last[:collapsible]
+      body_options = {}
+
+      if collapsible
+        final_class += ' collapse'
+        random_string = (0...8).map { (65 + rand(26)).chr }.join
+        body_options[:id] = "collapsible_#{random_string}"
+        final_title = [ expand_collapsible_label(body_options[:id]), args.last[:title]].compact.inject(:+)
+        args.last[:title] = sanitize(final_title, attributes: %w(href data-toggle)).html_safe
+      end
+
       panel(*args) do
-        content_tag :div, class: 'panel-body' do
+        body_options[:class] = final_class
+        content_tag :div, body_options do
           yield
         end
       end
